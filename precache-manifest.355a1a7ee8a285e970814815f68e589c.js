e62ed9ca10929f91ca1e73ddaa79e951b44ba4f8
vcatalasan <vcatalasan@gmail.com>
2020-02-29 18:34:22 -0800

    Updates

---
+++ precache-manifest.355a1a7ee8a285e970814815f68e589c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ce72705622f861e6e05ca0453f08b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b7e0547195d629fefa0d",
+    "url": "/monsters-rolodex/static/css/main.73c45c06.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e0547195d629fefa0d",
+    "url": "/monsters-rolodex/static/js/main.36477c3b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
